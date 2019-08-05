28a76c24a7d77b79ef68d673756212e9ff652c10
dvcolin <46186956+dvcolin@users.noreply.github.com>
2019-08-05 14:03:22 -0700

    Created todosData in App.js. Added state to App component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,44 @@
 import React from 'react';
 
+const todosData = [
+  {
+    task: 'Take out the garbage',
+    id: Date.now(),
+    completed: false,
+  },
+  {
+    task: 'Learn class components',
+    id: Date.now(),
+    completed: false,
+  },
+  {
+    task: 'Make dinner',
+    id: Date.now(),
+    completed: false,
+  },
+  {
+    task: 'Pay bills',
+    id: Date.now(),
+    completed: false,
+  },
+  {
+    task: 'Complete MVP',
+    id: Date.now(),
+    completed: false,
+  },
+]
+
+
 class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+  constructor() {
+    super();
+    this.state = {
+      todos: todosData,
+    }
+  }
   render() {
     return (
       <div>
